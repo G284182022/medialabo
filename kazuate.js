7bf5bca6d1825196c0b7448694b560d6f3d885ef
G284182022 <228418@st.takushoku-u.ac.jp>
2023-05-18 16:16:04 +0900

    数当てゲーム

--- kazuate.js
+++ kazuate.js
@@ -17,7 +17,21 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+  kaisua=(kaisu+1);
+  console.log(kaisua+"回目の予想"+yoso);
+if(kaisu<3){
+    if(yoso===kotae){
+        console.log("正解です.おめでとう!");
+        kaisu=3;
+    }if(yoso<kotae){
+        console.log("まちがい.答えはもっと大きいですよ.");
+    }if(yoso>kotae){
+        console.log("まちがい.答えはもっと小さいですよ.");
+    }
+}else{
+console.log("答えは"+kotae+"でした.既にゲームは終わっています.");
+}
+kaisu = kaisu + 1;
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
